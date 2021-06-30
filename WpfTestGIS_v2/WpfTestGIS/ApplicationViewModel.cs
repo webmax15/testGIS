2982378c6bb2dd5c70fe1ad3d37adbc725e00389
webmax15 <veber-maksim@mail.ru>
2021-06-30 14:34:23 +0700

    Add files via upload

---
+++ WpfTestGIS_v2/WpfTestGIS/ApplicationViewModel.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfTestGIS
+{
+    class ApplicationViewModel
+    {
+        public static List<MyPoint> pointsList = new List<MyPoint>();
+        public static List<MyLine> lineList = new List<MyLine>();
+        public static List<MyPolygon> polygonList = new List<MyPolygon>();
+
+        public void ReadFile(string path)
+        {
+            pointsList.Clear(); lineList.Clear(); polygonList.Clear();  //очищаю коллекции, чтобы не было объектов из других файлов
+
+            MainWindow.lineState = null;        //обнуляем перемен, отвечающую строку состояния 
+
+            try
+            {
+                using (StreamReader fileStream = new StreamReader(path, System.Text.Encoding.UTF8)) //начинаем считывать текстовый файл
+                {
+                    int countIteration = 0;
+                    int countError = 0;
+                    string line;
+                    while ((line = fileStream.ReadLine()) != null)  //считываем построчно файл, пока он не закончится
+                    {
+                        countIteration++;
+                        try
+                        {
+                            string[] splitLine = line.Split(' ');   //получаем массив координат
+                            if (splitLine.Length == 2)
+                            {
+                                pointsList.Add(new MyPoint(Convert.ToDouble(splitLine[0]), Convert.ToDouble(splitLine[1])));
+                            }
+                            else if (splitLine.Length == 4)
+                            {
+                                lineList.Add(new MyLine(Convert.ToDouble(splitLine[0]), Convert.ToDouble(splitLine[1]),
+                                                        Convert.ToDouble(splitLine[2]), Convert.ToDouble(splitLine[3])));
+                            }
+                            else if (splitLine.Length == 6)
+                            {
+                                polygonList.Add(new MyPolygon(Convert.ToDouble(splitLine[0]), Convert.ToDouble(splitLine[1]),
+                                                              Convert.ToDouble(splitLine[2]), Convert.ToDouble(splitLine[3]),
+                                                              Convert.ToDouble(splitLine[4]), Convert.ToDouble(splitLine[5])));
+                            }
+                            else
+                            {
+                                countError++;
+                            }
+                        }
+                        catch
+                        {
+                            countError++;
+                        }
+                    }
+                    if (countError == 0)
+                    {
+                        MainWindow.lineState = "Данные прочитаны полностью";
+                    }
+                    else
+                    {
+                        MainWindow.lineState = "Данные прочитаны частично";
+                    }
+
+                }
+            }
+            catch
+            {
+                MainWindow.lineState = "Файл не прочитан";
+            }
+            if (pointsList.Count == 0 && lineList.Count == 0 && polygonList.Count == 0)
+            {
+                MainWindow.lineState = "Файл не прочитан";
+            }
+        }
+
+    }
+}
---
+++ WpfTestGIS_v2/WpfTestGIS/MainWindow.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfTestGIS
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    /// 
+    public partial class MainWindow : Window
+    {
+        ApplicationViewModel applicationViewModel = new ApplicationViewModel();
+        public static string lineState = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            txtBoxFile.ToolTip = "Введите полный путь до файла и нажмите ENTER"; //подсказка
+        }
+
+        private void butOverview_Click(object sender, RoutedEventArgs e)
+        {
+            canvas.Children.Clear();        //очищаю все что нарисовано во ViewBox
+            var fileDialog = new System.Windows.Forms.OpenFileDialog();
+            fileDialog.Filter = "*.txt|*.txt";      //фильтр для выбора только тектовых файлов
+            var result = fileDialog.ShowDialog();   //окно выбора файла
+            switch (result)
+            {
+                case System.Windows.Forms.DialogResult.OK:
+                    var file = fileDialog.FileName;
+                    txtBoxFile.Text = file;
+                    break;
+                case System.Windows.Forms.DialogResult.Cancel:
+                default:
+                    txtBoxFile.Text = null;
+                    break;
+            }
+            DrawFigure();                   //вызываю метод где будут рисоваться объекты
+            TxtBoxState.Text = lineState;   //выводим результат работы с файлом
+        }
+
+        private void txtBoxFile_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                canvas.Children.Clear();        //очищаю все что нарисовано во ViewBox
+                DrawFigure();                   //вызываю метод где будут рисоваться объекты
+                TxtBoxState.Text = lineState;   //выводим результат работы с файлом
+            }
+        }
+
+        private void DrawFigure()
+        {
+            applicationViewModel.ReadFile(txtBoxFile.Text);         //считываем данные с файла
+            foreach (var point in ApplicationViewModel.pointsList)
+            {
+                Ellipse ellipse = new Ellipse();                    //через элипс я отобраю точку 
+                ellipse.Width = 5;
+                ellipse.Height = 5;
+                ellipse.Fill = Brushes.Black;
+                ellipse.Margin = new Thickness(point.X, point.Y, 0, 0);
+                canvas.Children.Add(ellipse);
+            }
+            foreach (var lineModel in ApplicationViewModel.lineList)
+            {
+                Line line = new Line();         //строим линию
+                line.X1 = lineModel.X1;
+                line.Y1 = lineModel.Y1;
+                line.X2 = lineModel.X2;
+                line.Y2 = lineModel.Y2;
+                line.Stroke = Brushes.Black;
+                canvas.Children.Add(line);
+            }
+            foreach (var polygonModel in ApplicationViewModel.polygonList)
+            {
+                Polygon polygon = new Polygon();            //строим полигон
+                List<Point> collection = new List<Point>();     //создаю коллекцию точек
+                collection.Add(new Point(polygonModel.Point1.X, polygonModel.Point1.Y));        
+                collection.Add(new Point(polygonModel.Point2.X, polygonModel.Point2.Y));
+                collection.Add(new Point(polygonModel.Point3.X, polygonModel.Point3.Y));
+                polygon.Points = new PointCollection(collection);
+                polygon.Stroke = Brushes.Black;
+                canvas.Children.Add(polygon);
+            }
+        }
+    }
+}
